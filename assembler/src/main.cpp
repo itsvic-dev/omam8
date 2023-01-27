2960c391a22f30477a775eed68e148e23f27a6cc
omame <me@omame.xyz>
2023-01-27 11:38:41 +0100

    asm: specify output file

--- assembler/src/main.cpp
+++ assembler/src/main.cpp
@@ -3,14 +3,18 @@
 // functions
 #include "parser.c"
 #include <cstdio>
-#include <iostream>
 #include <exception>
+#include <iostream>
 
-int main() {
+int main(int argc, char **argv) {
+  if (argc < 2) {
+    std::cout << "No output file specified.\n";
+    return 1;
+  }
   while (yyparse())
     ;
   Preprocessor &preprocessor = get_preprocessor();
   preprocessor.build_intermediate_rom();
-  preprocessor.save_rom("test.rom");
+  preprocessor.save_rom(argv[1]);
   return 0;
 }
