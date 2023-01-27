ec2cc8948bb5186199a4a64b96e5d343ca2e37e9
omame <me@omame.xyz>
2023-01-27 13:10:24 +0100

    emu: catch intentional throws

--- emulator/src/main.cpp
+++ emulator/src/main.cpp
@@ -1,6 +1,8 @@
 #include <core.h>
+#include <exception>
 #include <iostream>
 #include <rom.h>
+#include <stdexcept>
 
 int main(int argc, char **argv) {
   if (argc < 2) {
@@ -9,7 +11,19 @@ int main(int argc, char **argv) {
   }
 
   omam8::Core::init();
-  omam8::Core::load_rom(omam8::ROM::get_rom(argv[1]));
-  omam8::Core::start_loop();
+
+  try {
+    omam8::Core::load_rom(omam8::ROM::get_rom(argv[1]));
+  } catch (std::logic_error exc) {
+    std::cerr << "Failed to load ROM: " << exc.what() << "\n";
+    return 1;
+  }
+
+  try {
+    omam8::Core::start_loop();
+  } catch (std::logic_error exc) {
+    std::cerr << "Execution failed: " << exc.what() << "\n";
+    return 1;
+  }
   return 0;
 }
--- emulator/src/rom.cpp
+++ emulator/src/rom.cpp
@@ -11,37 +11,32 @@
 using namespace omam8::ROM;
 
 ROMData omam8::ROM::get_rom(std::string file) {
-  try {
-    std::ifstream fileStream(file, std::ios::binary);
-    if (!fileStream.is_open())
-      throw std::invalid_argument("file doesn't exist");
+  std::ifstream fileStream(file, std::ios::binary);
+  if (!fileStream.is_open())
+    throw std::invalid_argument("file doesn't exist");
 
-    std::vector<uint8_t> rawData;
+  std::vector<uint8_t> rawData;
 
-    std::for_each(std::istreambuf_iterator<char>(fileStream),
-                  std::istreambuf_iterator<char>(),
-                  [&rawData](const char c) { rawData.push_back(c); });
+  std::for_each(std::istreambuf_iterator<char>(fileStream),
+                std::istreambuf_iterator<char>(),
+                [&rawData](const char c) { rawData.push_back(c); });
 
-    fileStream.close();
+  fileStream.close();
 
-    if (rawData.size() < 0x8009)
-      throw std::length_error("ROM is smaller than expected");
+  if (rawData.size() < 0x8009)
+    throw std::length_error("ROM is smaller than expected");
 
-    if (memcmp(rawData.data(), "OMAM8ROM", 8) != 0)
-      throw std::invalid_argument("magic doesn't match");
+  if (memcmp(rawData.data(), "OMAM8ROM", 8) != 0)
+    throw std::invalid_argument("magic doesn't match");
 
-    uint8_t banks = rawData[8];
-    if (banks < 1 || banks > 64)
-      throw std::invalid_argument("bank amount is invalid");
+  uint8_t banks = rawData[8];
+  if (banks < 1 || banks > 64)
+    throw std::invalid_argument("bank amount is invalid");
 
-    if (rawData.size() < (9 + 0x4000 + (0x4000 * banks)))
-      throw std::length_error("ROM is smaller than expected by bank amount");
+  if (rawData.size() < (9 + 0x4000 + (0x4000 * banks)))
+    throw std::length_error("ROM is smaller than expected by bank amount");
 
-    uint8_t *data = rawData.data() + 9;
+  uint8_t *data = rawData.data() + 9;
 
-    return {static_cast<char>(banks), data};
-  } catch (...) {
-    std::cerr << "Invalid ROM file.\n";
-    std::rethrow_exception(std::current_exception());
-  }
+  return {static_cast<char>(banks), data};
 }
